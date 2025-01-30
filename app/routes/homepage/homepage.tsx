73633460392f34e65fd8a18a90c5b0f8d3721907
KaoM3 <rafaeldcbernandino@gmail.com>
2025-01-30 13:50:08 +0800

    fix: made accountSetup mobile responsive

---
+++ app/routes/homepage/homepage.tsx
@@ -0,0 +1,17 @@
+export default function HomePage(){
+    return (
+        <div className="flex h-full bg-custom-bg-white">
+            <nav className="w-96 bg-lasalle-dark-green hidden lg:block">
+
+            </nav>
+
+            <main className="flex-1">
+                <div className="w-[640px]"></div>
+            </main>
+
+            <div className="w-96 bg-lasalle-dark-green hidden md:block">
+
+            </div>
+        </div>
+    )
+}
