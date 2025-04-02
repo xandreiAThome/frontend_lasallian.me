61b1c9a71fd3e07620ff56d93febbb5e083e3eed
KaoM3 <rafaeldcbernandino@gmail.com>
2025-04-02 20:09:47 +0800

    feat: password reset request creation

---
+++ app/routes/auth/forgotPasswordRoute.tsx
@@ -0,0 +1,131 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { data, useFetcher, useNavigate } from "react-router";
+import { Button } from "~/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "~/components/ui/dialog";
+import { Input } from "~/components/ui/input";
+import api from "~/lib/api";
+import type { Route } from "./+types/RegisterRoute";
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const email = formData.get("email");
+
+  const errors: {
+    email?: string;
+    emailRed?: string;
+  } = {};
+
+  const valid =
+    typeof email === "string" ? email.match(/@dlsu\.edu\.ph$/) : null;
+
+  if (!valid) {
+    errors.email = "Invalid email address. Not a DLSU email";
+    errors.emailRed = "bg-red-200";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return data({ errors }, { status: 400 });
+  }
+
+  try {
+    const response = await api.post(`${process.env.API_KEY}/resetpassword/create`, {
+      email: email
+    });
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(
+        "Password Reset Creation Error:",
+        error.response?.data || error.message
+      );
+      return (error.response?.data)
+    } else {
+      console.error("Unexpected error:", error);
+    }
+  }
+}
+
+export default function ForgotPasswordRoute() {
+  const fetcher = useFetcher();
+  const navigate = useNavigate();
+  const errors = fetcher.data?.errors
+  const successMessage = fetcher.data?.message;
+  const [ isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  
+  // Use useEffect to handle dialog state changes based on fetcher.data
+  useEffect(() => {
+    if (fetcher.data?.errors === undefined && fetcher.data?.message) {
+      setIsDialogOpen(true); // Open the dialog only when there's no error and a success message exists
+    }
+  }, [fetcher.data]); // Only run when fetcher.data changes
+
+  return (
+    <fetcher.Form
+      method="post"
+      className="bg-custom-postcard-white w-full md:w-3/5 p-8 shadow-lg rounded-md"
+    >
+      <div className="flex flex-col items-center">
+        <p className="mb-4">Enter your email to receive a reset link.</p>
+        <Input
+          className={`bg-slate-50 mb-2 ${
+            errors?.emailRed ? errors.emailRed : ""
+          }`}
+          type="email"
+          name="email"
+          placeholder="Email Address"
+          required
+        ></Input>
+        {errors?.email ? (
+          <em className="text-red-500 mt-1">{errors.email}</em>
+        ) : null}
+        <Button
+          variant="link"
+          className="m-4 font-semibold text-lasalle-green text-base"
+          type="button"
+          onClick={() => navigate("/")}
+        >
+          Already have an account?
+        </Button>
+      </div>
+      <hr className="-mx-8" />
+      <Button
+            className="text-white w-full rounded-3xl mt-6 bg-lasalle-green text-lg h-12 self-center"
+            type="submit"
+            >
+            Send Reset Link
+        </Button>
+
+        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        {" "}
+        <DialogTrigger className="flex justify-center w-full"></DialogTrigger>
+        <DialogContent className="sm:max-w-[640px] overflow-y-auto max-h-screen justify-center">
+            <DialogHeader>
+            <DialogTitle>
+                <h1 className="text-2xl">Password Reset Link Sent!</h1>
+                <h4 className="text-base font-normal">
+                {successMessage}
+                </h4>
+            </DialogTitle>
+            </DialogHeader>{" "}
+            <DialogFooter>
+            <Button
+                className="text-white w-full rounded-3xl mt-6 bg-lasalle-green text-lg h-12"
+                onClick={() => setIsDialogOpen(false)}
+                >
+                OK!
+            </Button>
+            </DialogFooter>
+        </DialogContent>
+        </Dialog>
+    </fetcher.Form>
+  );
+}
---
+++ app/routes/auth/resetPasswordRoute.tsx
@@ -0,0 +1,166 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { data, redirect, useFetcher, useNavigate, useParams } from 'react-router';
+import ResetPasswordDialog from '~/components/resetPasswordComponents/ResetPasswordDialog';
+import { Button } from '~/components/ui/button';
+import { Input } from "~/components/ui/input";
+import api from "~/lib/api";
+import type { Route } from '../+types/accountSetupRoute';
+
+
+export async function action({ request }: Route.ActionArgs) {
+    const formData = await request.formData();
+    const email = formData.get("email");
+    const pass = formData.get("password");
+    const confirmPass = formData.get("confirm-password");
+  
+    const errors: {
+      password?: string;
+      passRed?: string;
+      confirmRed?: string;
+    } = {};
+  
+    const validPass =
+      typeof pass === "string"
+        ? pass.match(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+          )
+        : null;
+    
+    if (!validPass) {
+      errors.password =
+        "Password must contain one uppercase, lowercase, number and special character";
+      errors.passRed = "bg-red-200";
+      return data({ errors }, { status: 400 });
+    }
+  
+    if (pass !== confirmPass) {
+      errors.password = "Password do not match";
+      errors.confirmRed = "bg-red-200";
+    }
+  
+    if (Object.keys(errors).length > 0) {
+      return data({ errors }, { status: 400 });
+    }
+  
+    if (Object.keys(errors).length > 0) {
+      return data({ errors }, { status: 400 });
+    }
+  
+    try {
+      const response = await api.post(`${process.env.API_KEY}/user/register`, {
+        credentials: {
+          email: email,
+          password: pass,
+        },
+      });
+  
+      console.log("Registration successful:", response.data.session_token);
+      return redirect(
+        `/setup?token=${encodeURIComponent(response.data.session_token)}`
+      );
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(
+          "Registration error:",
+          error.response?.data || error.message
+        );
+      } else {
+        console.error("Unexpected error:", error);
+      }
+      // Handle error appropriately
+      // return { error: "Registration failed. Please try again." };
+    }
+  }
+  
+
+export default function ResetPassword() {
+    const fetcher = useFetcher();
+    const navigate = useNavigate();
+    const { resetPasswordId } = useParams<{resetPasswordId: string}>();
+    const errors: {
+        email?: string;
+        password?: string;
+        emailRed?: string;
+        passRed?: string;
+        confirmRed?: string;
+        emailUsed?: string;
+      } = {};
+
+    const [isDialogOpen, setIsDialogOpen] = useState(false); // State to control dialog visibility
+
+    // Function to open the dialog
+    const openDialog = () => {
+        setIsDialogOpen(true);
+    };
+
+    return (
+        <fetcher.Form
+            method="post"
+            className="bg-custom-postcard-white w-full md:w-3/5 p-8 shadow-lg rounded-md"
+        >
+            <div className="flex flex-col items-center">
+            <Input
+                className={`bg-slate-50 mb-6 ${
+                errors?.emailRed ? errors.emailRed : ""
+                }`}
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                required
+            ></Input>
+            {errors?.email ? (
+                <em className="text-red-500 -mt-6">{errors.email}</em>
+            ) : null}
+            {errors?.emailUsed ? (
+                <em className="text-red-500 -mt-6">{errors.emailUsed}</em>
+            ) : null}
+            <Input
+                className={`bg-slate-50 mb-4 ${
+                errors?.passRed ? errors.passRed : ""
+                }`}
+                type="password"
+                name="password"
+                placeholder="Password"
+                required
+            ></Input>
+            <Input
+                className={`bg-slate-50 ${
+                errors?.confirmRed ? errors.confirmRed : ""
+                }`}
+                type="password"
+                name="confirm-password"
+                placeholder="Confirm Password"
+                required
+            ></Input>
+            {errors?.password ? (
+                <em className="text-red-500">{errors?.password}</em>
+            ) : null}
+            <Button
+                variant="link"
+                className="m-4 font-semibold text-lasalle-green text-base"
+                type="button"
+                onClick={openDialog}
+            >
+                Already have an account?
+            </Button>
+            </div>
+            <hr className="-mx-8" />
+            <Button
+            className="text-white w-full rounded-3xl mt-6 bg-lasalle-green text-lg h-12"
+            type="submit"
+            >
+            Create Account
+            </Button>
+            {
+                isDialogOpen && (
+                    <ResetPasswordDialog
+                        setOpen={setIsDialogOpen} // You can use this to close the dialog
+                        buttonProp={<Button>Click Me to Open Dialog</Button>} // Example trigger button for the dialog
+                        message="Your password reset link has been sent." // Optional message prop
+                    />
+                )
+            }
+        </fetcher.Form>
+    );
+}
